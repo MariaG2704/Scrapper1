ad65676b6a2573ff86753040eca1189270f5f0c8
ccolin <colin-nguyen@uiowa.edu>
2024-09-05 13:24:38 -0500

    Cleaned up using Google Coding Standards

--- src/edu/uiowa/cs/warp/WorkLoadDescription.java
+++ src/edu/uiowa/cs/warp/WorkLoadDescription.java
@@ -1,18 +1,13 @@
-/**
- * 
- */
 package edu.uiowa.cs.warp;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.Scanner;
 /**
  * Reads the input file, whose name is passed as input parameter to the constructor, and builds a
  * Description object based on the contents. Each line of the file is an entry (string) in the
  * Description object.
- * 
+
  * @author sgoddard
  * @version 1.8 Fall 2024
  */
@@ -58,45 +53,44 @@ public class WorkLoadDescription extends VisualizationObject {
     this.inputFileName = gf.getGraphFileName();
     description = new Description(inputGraphString);
   }
-  
+  /**
+   * Reads a graph txt file and outputs flows in alphabetical order.
+   * Leverages existing file manager structure
+
+   * @param args no intended input
+   */
   public static void main(String[] args) {
-	  // Try-catch for non-existing graph files
-	  try {
-		  // Open and set up file to be read
-		  File currObj = new File("StressTest.txt");
-		  Scanner reader = new Scanner(currObj);
-		  ArrayList<String> flowArr = new ArrayList<String>();
-		  // Get filename from first line, remove {
-		  String data = reader.nextLine();
-		  System.out.println(data.substring(0, data.length()-2));
-		  // Gather all flows into flowArr
-		  while (reader.hasNextLine()) {
-			  data = reader.nextLine();
-			  // End of flow data
-			  if (data.equals("}")){
-				  continue;
-			  } else {
-				  flowArr.add(data);
-			  }
-		  }
-		  reader.close();
-		  // Sort array alphabetically, only considering the names and not the data portion
-		  String[] flowArray = flowArr.toArray(new String[0]);
-		  Arrays.sort(flowArray, (s1, s2) -> {
-			  String prefix1 = s1.split(" ")[0];
-			  String prefix2 = s2.split(" ")[0];
-			  return prefix1.compareTo(prefix2);
-		  });
-		  // Print all flows in formatted way
-		  int count = 1;
-		  for (String s: flowArray) {
-			  System.out.print("Flow " + count + ": ");
-			  System.out.println(s);
-			  count++;
-		  }
-	  } catch (FileNotFoundException e) {
-		  System.out.println("StressText.txt not found in assumed directory");
-		  e.printStackTrace();
-	  }
+    // Open and read file using FM and Scanner
+    InputGraphFile graphFile = new InputGraphFile(new FileManager());
+    String currObj = graphFile.readGraphFile("StressTest.txt");
+    Scanner reader = new Scanner(currObj);
+    ArrayList<String> flowArr = new ArrayList<String>();
+    // Get filename from first line, remove {
+    String data = reader.nextLine();
+    System.out.println(data.substring(0, data.length() - 2));
+    // Gather all flows into flowArr
+    while (reader.hasNextLine()) {
+      data = reader.nextLine();
+      // End of flow data
+      if (data.equals("}")) {
+        continue;
+      }
+      flowArr.add(data);
+    }
+    reader.close();
+    // Sort array alphabetically, only considering the names and not the data portion
+    String[] sortedArray = flowArr.toArray(new String[0]);
+    Arrays.sort(sortedArray, (s1, s2) -> {
+      String prefix1 = s1.split(" ")[0];
+      String prefix2 = s2.split(" ")[0];
+      return prefix1.compareTo(prefix2);
+    });
+    // Print all flows in formatted way
+    int count = 1;
+    for (String flow : sortedArray) {
+      System.out.print("Flow " + count + ": ");
+      System.out.println(flow);
+      count++;
+    }
   }
 }
