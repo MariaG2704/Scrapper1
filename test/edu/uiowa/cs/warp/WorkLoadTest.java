63e8a21b545d628f264433892624fde1216ae496
ccolin <colin-nguyen@uiowa.edu>
2024-09-25 09:59:13 -0500

    junit gen test

---
+++ test/edu/uiowa/cs/warp/WorkLoadTest.java
@@ -0,0 +1,159 @@
+/**
+ * 
+ */
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+class WorkLoadTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#addFlow(java.lang.String)}.
+	 */
+	@Test
+	void testAddFlow() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#addNodeToFlow(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testAddNodeToFlow() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getFlowPriority(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testGetFlowPriorityStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getFlowPriority(java.lang.String)}.
+	 */
+	@Test
+	void testGetFlowPriorityString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#setFlowPriority(java.lang.String, java.lang.Integer)}.
+	 */
+	@Test
+	void testSetFlowPriority() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#setFlowDeadline(java.lang.String, java.lang.Integer)}.
+	 */
+	@Test
+	void testSetFlowDeadline() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getFlowDeadline(java.lang.String)}.
+	 */
+	@Test
+	void testGetFlowDeadline() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getFlowTxAttemptsPerLink(java.lang.String)}.
+	 */
+	@Test
+	void testGetFlowTxAttemptsPerLink() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#setFlowsInRMorder()}.
+	 */
+	@Test
+	void testSetFlowsInRMorder() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getNodeNamesOrderedAlphabetically()}.
+	 */
+	@Test
+	void testGetNodeNamesOrderedAlphabetically() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getFlowNames()}.
+	 */
+	@Test
+	void testGetFlowNames() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getNodeIndex(java.lang.String)}.
+	 */
+	@Test
+	void testGetNodeIndex() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getNodesInFlow(java.lang.String)}.
+	 */
+	@Test
+	void testGetNodesInFlow() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getHyperPeriod()}.
+	 */
+	@Test
+	void testGetHyperPeriod() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getTotalTxAttemptsInFlow(java.lang.String)}.
+	 */
+	@Test
+	void testGetTotalTxAttemptsInFlow() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#getNumTxAttemptsPerLink(java.lang.String)}.
+	 */
+	@Test
+	void testGetNumTxAttemptsPerLink() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.uiowa.cs.warp.WorkLoad#maxFlowLength()}.
+	 */
+	@Test
+	void testMaxFlowLength() {
+		fail("Not yet implemented");
+	}
+
+}
