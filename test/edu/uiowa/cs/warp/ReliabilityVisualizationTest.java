0810898694a2e1050fd32e721ea11b47bd30f6db
Nancy Nahra <nancy.t.nahra@gmail.com>
2024-11-18 23:49:12 -0600

    Attempted to add testDisplayVisualization and created a ReliabilityVisualizationTest.java

---
+++ test/edu/uiowa/cs/warp/ReliabilityVisualizationTest.java
@@ -0,0 +1,28 @@
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.Test;
+
+class ReliabilityVisualizationTest {
+	
+
+	private static final String String = null;
+	private GuiVisualization gVis;
+
+	/*
+	 public GuiVisualization displayVisualization() {
+		return new GuiVisualization(createTitle(), createColumnHeader(), createVisualizationData());
+	}
+	 */
+	
+	@Test
+	void testDisplayVisualization() {
+		
+		 gVis = new GuiVisualization(String s, String[] a, String[][] b);
+		
+		
+	}
+		
+	
+}
